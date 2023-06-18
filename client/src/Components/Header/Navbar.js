b6f94a69a54f5961fc4bac057026eb770dee079c
Harsh <hk3711750@gmail.com>
2023-06-18 17:59:38 +0530

    slider of deals created footer created

--- client/src/Components/Header/Navbar.js
+++ client/src/Components/Header/Navbar.js
@@ -11,11 +11,11 @@ const Navbar = () => {
         <nav>
             <div className='left'>
                <div className='navlogo'>
-               <img src='' alt='Logo'/>
+               <img src="	" alt='Logo'/>
                </div>
                <div>
                 <div className='nav_searchbar'>
-                    <input type = "text" name = "" id ="" />
+                    <input type = "text" name = "" id ="" placeholder ="Search Your Products" />
                     <div className='search_icon'>
                         <SearchIcon/>
                     </div>
@@ -24,7 +24,7 @@ const Navbar = () => {
             </div>
             <div className='right'>
                 <div className='nav_btn'>
-                    <a href='#'>Signin</a>
+                    <a href='#'>Sign in</a>
                 </div>
                 <div className='cart_btn'>
                     <Badge badgeContent={4} color ="primary">

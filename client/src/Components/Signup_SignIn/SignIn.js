a146a36b5e67fc4bdb91371d912c3f6cb4d4b522
Harsh <harshsantoshi05@gmail.com>
2023-06-25 10:24:41 +0530

    formatted sigin

--- client/src/Components/Signup_SignIn/SignIn.js
+++ client/src/Components/Signup_SignIn/SignIn.js
@@ -1,51 +1,50 @@
 import React, { useState } from 'react'
 import './signin.css'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function SignIn() {
-    const [logdata , setData]= useState({
-        email:"",
-        password:""
+    const [logdata, setData] = useState({
+        email: "",
+        password: ""
     });
     // console.log(data);
-    const addData = (e)=>{
-        const {name , value}  = e.target;
-        setData(()=>{
+    const addData = (e) => {
+        const { name, value } = e.target;
+        setData(() => {
             return {
                 ...logdata,
-                [name]:value
+                [name]: value
             }
         })
     }
-    const sendData = async(e)=>{
+    const sendData = async (e) => {
         e.preventDefault();
-        const{email , password} = logdata;
-        const res = await fetch("/login" ,{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
+        const { email, password } = logdata;
+        const res = await fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
-               email,password
+            body: JSON.stringify({
+                email, password
             })
         });
 
-        const  data = await res.json();
+        const data = await res.json();
         console.log(data);
-        if(res.status === 400 || !data){
+        if (res.status === 400 || !data) {
             console.log("Invalid Deatails");
             toast.warn("Invalid Details!", {
                 position: "top-center"
             });
         }
-        else{
+        else {
             console.log("Valid Data");
             toast.success("User Logged In", {
                 position: "top-center"
             });
-            
-            setData({...logdata , email :"" , password: ""});
+            setData({ ...logdata, email: "", password: "" });
         }
     }
     return (
@@ -59,27 +58,27 @@ function SignIn() {
                         <h1>Sign-In</h1>
                         <div className='form_data'>
                             <label htmlFor='email' >Email</label>
-                            <input type='text' 
-                            onChange={addData}
-                            value={logdata.email}
-                            name="email" id='email' />
+                            <input type='text'
+                                onChange={addData}
+                                value={logdata.email}
+                                name="email" id='email' />
                         </div>
                         <div className='form_data'>
                             <label htmlFor='password' >Password</label>
                             <input type='password'
-                            onChange={addData}
-                            value={logdata.password}
-                            name="password" id='password' placeholder='At least 6 characters' />
+                                onChange={addData}
+                                value={logdata.password}
+                                name="password" id='password' placeholder='At least 6 characters' />
                         </div>
                         <button className='signin_btn' onClick={sendData}>Login</button>
                     </form>
                 </div>
                 <div className='create_accountinfo'>
                     <p>New to Amazon ?</p>
-                   <NavLink to= "/signup" ><button>Create your Amazon Account</button></NavLink>
+                    <NavLink to="/signup" ><button>Create your Amazon Account</button></NavLink>
                 </div>
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </section>
     )
 }

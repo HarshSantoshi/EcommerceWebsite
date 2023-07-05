269d738638016851efa2b7ca1649f166450f01dc
Harsh <harshsantoshi05@gmail.com>
2023-07-05 22:35:28 +0530

    Added Empty Cart image

--- client/src/Components/buynow/Buynow.js
+++ client/src/Components/buynow/Buynow.js
@@ -1,13 +1,14 @@
 import { Divider } from '@mui/material';
-import {React,useEffect, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import "./buynow.css";
 import Option from "./Option";
 import Right from "./Right";
 import Subtotal from "./Subtotal";
+import EmptyCart from '../image/EmptyCart.jpg'
 
 const Buynow = () => {
     const [cartdata, setCartdata] = useState("");
-    
+
     const getdatabuy = async () => {
         const res = await fetch("/cartdetails", {
             method: "GET",
@@ -38,40 +39,41 @@ const Buynow = () => {
                         <p>Select all items</p>
                         <span className='leftbuyprice'>Price</span>
                         <Divider />{
-                            cartdata.map((e,ind) => {
+                            cartdata.map((e, ind) => {
                                 return (
                                     <>
-                                            <div className="item_container" key={ind}>
-                                                <img src={e.detailUrl} alt="imgitem" />
-                                                <div className="item_details">
-                                                    <h3>{e.title.longTitle}</h3>
-                                                    <h3>{e.title.shortTitle}</h3>
-                                                    <h3 className="diffrentprice">{e.price.cost}.00</h3>
-                                                    <p className="unusuall">Usually dispatched in 8 days.</p>
-                                                    <p>Eligible for FREE Shipping</p>
-                                                    <img src="https://m.media-amazon.com/images/G/31/marketing/fba/fba-badge_18px-2x._CB485942108_.png" alt="logo" />
-                                                    <Option deletedata={e.id} get={getdatabuy} />
-                                                </div>
-                                                <h3 className="item_price">{e.price.cost}.00</h3>
+                                        <div className="item_container" key={ind}>
+                                            <img src={e.detailUrl} alt="imgitem" />
+                                            <div className="item_details">
+                                                <h3>{e.title.longTitle}</h3>
+                                                <h3>{e.title.shortTitle}</h3>
+                                                <h3 className="diffrentprice">{e.price.cost}.00</h3>
+                                                <p className="unusuall">Usually dispatched in 8 days.</p>
+                                                <p>Eligible for FREE Shipping</p>
+                                                <img src="https://m.media-amazon.com/images/G/31/marketing/fba/fba-badge_18px-2x._CB485942108_.png" alt="logo" />
+                                                <Option deletedata={e.id} get={getdatabuy} />
                                             </div>
-                                            <Divider />
-                                        </>
+                                            <h3 className="item_price">{e.price.cost}.00</h3>
+                                        </div>
+                                        <Divider />
+                                    </>
                                 )
                             })
                         }
 
-                        
+
                         <Subtotal iteam={cartdata} />
                     </div>
-                    <Right iteam={cartdata}/>
+                    <Right iteam={cartdata} />
                 </div>
-            </div> : 
-            <div style={{minHeight:200}}>
-                <h1 style={{marginTop:200 , textAlign:'center'}}>
-                    No Item in the cart
-                </h1>
+            </div> :
+                <div style={{ minHeight: 'auto' }}>
+                    <div style={{ minHeight: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' , marginTop:100}}>
+                        <img src={EmptyCart} alt="img" />
+                    </div>
 
-            </div>
+
+                </div>
         }
 
         </>
